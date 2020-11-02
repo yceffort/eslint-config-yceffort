0f20b0e31acc8a524a86a36b7c1f2f3b8ae14e60
yceffort <yceffort@gmail.com>
2020-11-02 20:41:04 +0900

    🤪 필요없는 async 삭제

--- test/javascript/culry/index.test.js
+++ test/javascript/culry/index.test.js
@@ -5,26 +5,24 @@ const RULE_ID = "curly";
 
 const eslint = new ESLint({ baseConfig: config });
 
-(async () => {
-  describe("eslint-config-yceffort curly", function () {
-    it("right curly", async function () {
-      const [result] = await eslint.lintFiles([`${__dirname}/curly.right.js`]);
+describe("eslint-config-yceffort curly", function () {
+  it("right curly", async function () {
+    const [result] = await eslint.lintFiles([`${__dirname}/curly.right.js`]);
 
-      const errors = result.messages.some(
-        (message) => message.ruleId === RULE_ID
-      );
+    const errors = result.messages.some(
+      (message) => message.ruleId === RULE_ID
+    );
 
-      expect(errors).toBe(false);
-    });
-
-    it("wrong curly", async function () {
-      const [result] = await eslint.lintFiles([`${__dirname}/curly.wrong.js`]);
-
-      const errors = result.messages.some(
-        (message) => message.ruleId === RULE_ID
-      );
-
-      expect(errors).toBe(true);
-    });
+    expect(errors).toBe(false);
   });
-})();
+
+  it("wrong curly", async function () {
+    const [result] = await eslint.lintFiles([`${__dirname}/curly.wrong.js`]);
+
+    const errors = result.messages.some(
+      (message) => message.ruleId === RULE_ID
+    );
+
+    expect(errors).toBe(true);
+  });
+});
